f431803a24563260a129231b3f209da213f5d757
Valentine Mahembe <mahembevalentine@gmail.com>
2024-03-04 20:49:41 +0200

    Eslint corrections and checks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-	const task = false;
-	const task2 = true;
+  const task = false;
+  const task2 = true;
 
-	if (trueOrFalse) {
-	}
+  // eslint-disable-next-line no-empty
+  if (trueOrFalse) {
+  }
 
-	return [task, task2];
+  return [task, task2];
 }
