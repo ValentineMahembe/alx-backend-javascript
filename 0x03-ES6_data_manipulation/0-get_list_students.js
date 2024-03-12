0d89cf895e7be4b03b5ccd8c51ef9b1e6143f0d7
Valentine Mahembe <mahembevalentine@gmail.com>
2024-03-12 12:21:35 +0200

    0 correction

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,8 +2,8 @@ function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Colombia' },
-    { is: 5, firstname: 'Serena', location: 'San Francisco' }
-  ]
+    { is: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
 }
 
 export default getListStudents;
