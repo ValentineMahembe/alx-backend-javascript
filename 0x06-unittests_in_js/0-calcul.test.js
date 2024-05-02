ba8a7c2120ce91a5d877fcbf76f6e7c766fc2a45
Valentine Mahembe <mahembevalentine@gmail.com>
2024-05-02 14:59:14 +0000

    all

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,21 @@
-// 0-calcul.test.js
 const assert = require('assert');
+const {it, describe} = require('mocha');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should return the sum of two rounded numbers', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+describe('calculateNumber', () => {
+    it('checking if numbers round', () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('checking if numbers round 2nd round', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('checking if numbers round 3nd round', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('checking if numbers round 4nd round', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it('checking negative return', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
+      });
 });
