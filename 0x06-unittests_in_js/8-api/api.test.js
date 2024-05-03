2a09cadd0c5b1e2c3f97990378cc3ec574b049f3
Valentine Mahembe <mahembevalentine@gmail.com>
2024-05-03 10:03:05 +0000

    corrections

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,51 +1,13 @@
 const request = require('request');
 const { expect } = require('chai');
 
-const app = require('./api');
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index page', function() {
-  it('should return status code 200', function(done) {
-    request.get('http://localhost:7865/', function(error, response) {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct result', function(done) {
-    request.get('http://localhost:7865/', function(error, response, body) {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('should return status code 404 for invalid routes', function(done) {
-    request.get('http://localhost:7865/invalid', function(error, response) {
-      expect(response.statusCode).to.equal(404);
-      done();
-    });
-  });
-
-  it('should return content type as text/html', function(done) {
-    request.get('http://localhost:7865/', function(error, response) {
-      expect(response.headers['content-type']).to.include('text/html');
-      done();
-    });
-  });
-
-  it('should respond within a reasonable time frame', function(done) {
-  const startTime = Date.now();
-  request.get('http://localhost:7865/', function(error, response) {
-    const endTime = Date.now();
-    const responseTime = endTime - startTime;
-    expect(responseTime).to.be.lessThan(3000); // Assuming response time should be less than 3 seconds
-    done();
-  });
-});
-
-
-  it('should return a non-empty response body', function(done) {
-    request.get('http://localhost:7865/', function(error, response, body) {
-      expect(body).to.not.be.empty;
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
