13257143d508f2ed7c356e6da65d40380d9f14fd
Valentine Mahembe <mahembevalentine@gmail.com>
2024-05-03 09:03:30 +0000

    8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,49 @@
+const request = require('request');
+const { expect } = require('chai');
+
+const app = require('./api');
+
+describe('Index page', function() {
+  it('should return status code 200', function(done) {
+    request.get('http://localhost:7865/', function(error, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct result', function(done) {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should return status code 404 for invalid routes', function(done) {
+    request.get('http://localhost:7865/invalid', function(error, response) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('should return content type as text/html', function(done) {
+    request.get('http://localhost:7865/', function(error, response) {
+      expect(response.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+
+  it('should respond within a reasonable time frame', function(done) {
+    this.timeout(5000); // Increase timeout for this test
+    request.get('http://localhost:7865/', function(error, response) {
+      expect(response.elapsedTime).to.be.lessThan(3000); // Assuming response time should be less than 3 seconds
+      done();
+    });
+  });
+
+  it('should return a non-empty response body', function(done) {
+    request.get('http://localhost:7865/', function(error, response, body) {
+      expect(body).to.not.be.empty;
+      done();
+    });
+  });
+});
